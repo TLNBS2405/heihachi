f0d7df7215691245e7c45e59f751c3210891a6dc
lnguy <lnguyen2405@gmail.com>
2023-11-17 14:21:28 +0100

    Add pytest config

--- src/module/test/test_character_importer.py
+++ src/module/test/test_character_importer.py
@@ -1,11 +1,14 @@
-import unittest, json
+import unittest, json, os
 from src.module import character_importer
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 class MyTestCase(unittest.TestCase):
 
     def test_character_importer(self):
-        with open("azu.json") as azu:
+        azu_json = dir_path + "/azu.json"
+        with open(azu_json) as azu:
             azu_meta = json.load(azu)
             azucena = character_importer.import_character(azu_meta)
             self.assertEqual(azucena.name, "Azucena")
