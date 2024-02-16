17c4a56c19f8a666e54e08f86bcef1a848647f73
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2024-02-16 13:23:24 -0500

    Fix command tree issues by reverting to subclassing discord.Client

--- src/heihachi/bot.py
+++ src/heihachi/bot.py
@@ -13,27 +13,25 @@ from heihachi.embed import get_frame_data_embed
 logger = logging.getLogger("main")
 
 
-class FrameDataBot(discord.ext.commands.Bot):
+class FrameDataBot(discord.Client):
     def __init__(
         self,
-        command_prefix: str,
         framedb: FrameDb,
         frame_service: FrameService,
         config: Configurator,
-        description: str | None = None,
     ) -> None:
         intents = discord.Intents.default()
         intents.message_content = True
 
-        super().__init__(command_prefix, description=description, intents=intents)
+        super().__init__(intents=intents)
         self.framedb = framedb
         self.frame_service = frame_service
         self.config = config
         self.synced = False
+        self.tree = discord.app_commands.CommandTree(self)
 
-        self._add_bot_commands()  # TODO: fix bot slash commands not working in Discord
-        logger.debug(f"Bot commands added: {[cmd.name for cmd in self.commands]}")
-        logger.debug(f"Bot command tree: {self.tree.get_commands()}")
+        self._add_bot_commands()
+        logger.debug(f"Bot command tree: {[command.name for command in self.tree.get_commands()]}")
 
     async def on_ready(self) -> None:
         await self.wait_until_ready()
@@ -87,10 +85,10 @@ class FrameDataBot(discord.ext.commands.Bot):
     def _character_command_factory(self, name: str) -> Callable[[discord.Interaction, str], Coroutine[Any, Any, None]]:
         "A factory function to create /character command functions"
 
-        async def _character_command(ctx: discord.ext.commands.Context, move: str) -> None:
-            if not (self.is_user_blacklisted(str(ctx.interaction.user.id)) or self.is_author_newly_created(ctx.interaction)):
+        async def _character_command(interaction: discord.Interaction, move: str) -> None:
+            if not (self.is_user_blacklisted(str(interaction.user.id)) or self.is_author_newly_created(interaction)):
                 embed = get_frame_data_embed(self.framedb, self.frame_service, name, move)
-                await ctx.send(embed=embed, ephemeral=False)
+                await interaction.response.send_message(embed=embed, ephemeral=False)
 
         return _character_command
 
@@ -98,10 +96,12 @@ class FrameDataBot(discord.ext.commands.Bot):
         "Add all frame commands to the bot"
 
         @self.tree.command(name="fd", description="Frame data from a character move")
-        async def _frame_data_cmd(ctx: discord.ext.commands.Context, character_name_query: str, move_query: str) -> None:
-            if not (self.is_user_blacklisted(str(ctx.interaction.user.id)) or self.is_author_newly_created(ctx.interaction)):
+        async def _frame_data_cmd(interaction: discord.Interaction, character: str, move: str) -> None:
+            character_name_query = character
+            move_query = move
+            if not (self.is_user_blacklisted(str(interaction.user.id)) or self.is_author_newly_created(interaction)):
                 embed = get_frame_data_embed(self.framedb, self.frame_service, character_name_query, move_query)
-                await ctx.send(embed=embed, ephemeral=False)
+                await interaction.response.send_message(embed=embed, ephemeral=False)
 
         for character in CharacterName:
             char_name = character.value
@@ -112,15 +112,13 @@ class FrameDataBot(discord.ext.commands.Bot):
         if self.config.feedback_channel_id and self.config.action_channel_id:
 
             @self.tree.command(name="feedback", description="Send feedback incase of wrong data")
-            async def _feedback_cmd(ctx: discord.ext.commands.Context, message: str) -> None:
-                if not (
-                    self.is_user_blacklisted(str(ctx.interaction.user.id)) or self.is_author_newly_created(ctx.interaction)
-                ):
+            async def _feedback_cmd(interaction: discord.Interaction, message: str) -> None:
+                if not (self.is_user_blacklisted(str(interaction.user.id)) or self.is_author_newly_created(interaction)):
                     try:
                         feedback_message = "Feedback from **{}** with ID **{}** in **{}** \n- {}\n".format(
-                            str(ctx.interaction.user.name),
-                            ctx.interaction.user.id,
-                            ctx.interaction.guild,
+                            str(interaction.user.name),
+                            interaction.user.id,
+                            interaction.guild,
                             message,
                         )
                         try:
@@ -135,7 +133,7 @@ class FrameDataBot(discord.ext.commands.Bot):
                     except Exception as e:
                         result = embed.get_error_embed(f"Feedback couldn't be sent, caused by: {str(e)}")
 
-                    await ctx.send(embed=result, ephemeral=False)
+                    await interaction.response.send_message(embed=result, ephemeral=False)
         else:
             logger.warning("Feedback or Action channel ID is not set. Disabling feedback command.")
 
--- src/main.py
+++ src/main.py
@@ -17,15 +17,14 @@ from heihachi.bot import FrameDataBot, periodic_function
 "How often to update the bot's frame data from the external service and write to file."
 UPDATE_INTERVAL_SEC = 3600
 
-logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("main")
-logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
     "%(asctime)s %(levelname)s     %(module)s:%(funcName)s:%(lineno)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
 )
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
+logger.setLevel(logging.INFO)
 
 
 def main() -> None:
@@ -47,7 +46,7 @@ def main() -> None:
         framedb = FrameDb()
         framedb.refresh(frame_service, export_dir_path, _format)
         logger.info(f"Frame data loaded from service {frame_service.name} and written to {export_dir_path} as {_format}")
-        hei = FrameDataBot("/", framedb, frame_service, config)
+        hei = FrameDataBot(framedb, frame_service, config)
 
     except Exception as e:
         logger.error(f"Failed to initialize bot: {e}")
