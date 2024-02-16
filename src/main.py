315ea77b67b25565c1171852eb3481326739c1fe
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2024-02-16 13:57:04 -0500

    Move static files out of src

--- src/main.py
+++ src/main.py
@@ -2,17 +2,17 @@
 
 """The entry point for the bot."""
 
+import argparse
 import logging
 import os
 import sched
-import sys
 import threading
 import time
 from typing import Any, Callable, Tuple
 
 from frame_service import Wavu
 from framedb import FrameDb
-from heihachi import configurator
+from heihachi import Configurator
 from heihachi.bot import FrameDataBot
 
 "How often to update the bot's frame data from the external service and write to file."
@@ -36,18 +36,34 @@ def periodic_function(scheduler: sched.scheduler, interval: float, function: Cal
         scheduler.run()
 
 
+def get_argparser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description="Heihachi bot")
+    parser.add_argument("config_file", type=str, help="Path to the config file")
+    parser.add_argument(
+        "--export_dir",
+        type=str,
+        default=os.path.join(os.getcwd(), "json_movelist"),
+        help="Path to the directory to export frame data to",
+    )
+    parser.add_argument("--format", type=str, default="json", help="Format to export frame data to")
+    return parser
+
+
 def main() -> None:
+    parser = get_argparser()
+    args = parser.parse_args()
+    config_file_path = args.config_file
+    export_dir_path = args.export_dir
+    _format = args.format
+
     # retrieve config
     try:
-        config = configurator.Configurator.from_file(sys.argv[1])  # TODO: potentially use argparse
+        config = Configurator.from_file(config_file_path)
         assert config is not None
-        logger.info(f"Config file loaded from {sys.argv[1]}")
+        logger.info(f"Config file loaded from {config_file_path}")
     except FileNotFoundError:
-        logger.error(f"Config file not found at {sys.argv[1]}. Exiting...")
+        logger.error(f"Config file not found at {config_file_path}. Exiting...")
         exit(1)
-
-    export_dir_path = os.path.join(os.getcwd(), "json_movelist")
-    _format = "json"
 
     # initialize bot
     try:
